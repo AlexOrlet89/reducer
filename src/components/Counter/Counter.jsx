bac113040348ffe0d7c285d1d941f124ea4e8830
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-11 16:18:02 -0700

    colors are now in reduce function

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,47 +2,54 @@ import { useReducer } from 'react';
 import { useEffect, useState } from 'react';
 import styles from './Counter.css';
 
-const initialCount = 10;
-
-const countReducer = (state, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      console.log('increment', state);
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    case 'RESET':
-      return (state = 0);
-    default:
-      throw new Error(`Action type unsupported`);
-  }
-};
-
 const colors = {
   yellow: 'rgb(236, 222, 153)',
   green: 'rgb(52, 211, 153)',
   red: 'rgb(239, 68, 68)',
 };
 
+const initialCount = { count: 10, color: colors.green };
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      console.log('increment', state);
+      return { count: state.count + 1, color: state.color };
+    case 'DECREMENT':
+      return { count: state.count - 1, color: state.color };
+    case 'RESET':
+      return { count: 0, color: state.color };
+    case 'CHANGE_COLOR':
+      return {
+        count: state.count,
+        color: action.payload.color,
+      };
+    default:
+      throw new Error(`Action type unsupported`);
+  }
+};
+
 export default function Counter() {
   // const [count, setCount] = useState(0);
   const [count, dispatch] = useReducer(countReducer, initialCount);
-  const [currentColor, setCurrentColor] = useState(colors.yellow);
-  console.log(count);
+  // const [currentColor, setCurrentColor] = useState(colors.green);
+  // const [color, dispatch] = useReducer(colorReducer, initialColor);
+
+  console.log({ count });
 
   useEffect(() => {
-    if (count === 0) {
-      setCurrentColor(colors.yellow);
+    if (count.count === 0) {
+      dispatch({ type: 'CHANGE_COLOR', payload: { color: colors.yellow } });
     }
 
-    if (count > 0) {
-      setCurrentColor(colors.green);
+    if (count.count > 0) {
+      dispatch({ type: 'CHANGE_COLOR', payload: { color: colors.green } });
     }
 
-    if (count < 0) {
-      setCurrentColor(colors.red);
+    if (count.count < 0) {
+      dispatch({ type: 'CHANGE_COLOR', payload: { color: colors.red } });
     }
-  }, [count]);
+  }, [count.count]);
 
   const increment = () => {
     dispatch({ type: 'INCREMENT' });
@@ -58,7 +65,7 @@ export default function Counter() {
 
   return (
     <main className={styles.main}>
-      <h1 style={{ color: currentColor }}>{count}</h1>
+      <h1 style={{ color: count.color }}>{count.count}</h1>
       <div>
         <button
           type="button"
